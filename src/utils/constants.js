0153f521b256e8716daec4e4c8241aa780747af2
rahulvirani03 <rahulsvirani25@gmail.com>
2022-06-03 15:42:33 +0530

    API end point changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ import { UserOutlined, SolutionOutlined, FileJpgOutlined, SmileOutlined } from '
 const LOCAL_URL ='http://localhost:4000'
 
 const DEPLOYED_URL='https://chatapp-websocket-backend.herokuapp.com'
-export const URL =LOCAL_URL;
+export const URL =DEPLOYED_URL;
 
 
 export const VARIABLE_NAME = "VARIABLE_NAME";
