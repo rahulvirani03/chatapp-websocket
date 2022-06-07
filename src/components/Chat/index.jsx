142b3190ff32b3f4e11ad21c09162e8665bae9e5
rahulvirani03 <rahulsvirani25@gmail.com>
2022-06-07 12:43:52 +0530

    changes to chat UI

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useHistory } from "react-router-dom";
-import { Container, Text, Button } from "@components/custom";
+import { Container, Text, Button, Title, FlexContainer } from "@components/custom";
 import styled from "styled-components";
 import { api } from "@api";
 import { colors, styles } from "@themes";
-import { Avatar, Form } from "antd";
+import { Avatar, Form,message,Modal} from "antd";
 import ScrollableFeed from "react-scrollable-feed";
 import { SendOutlined } from "@ant-design/icons";
+import { AVARTAR_SRC } from "@utils/constants";
 
+message.config({
+  icons:{
+    info:undefined
+  }
+})
 const ChatContaier = styled(Container)`
   display: grid;
+
   width: 100%;
   min-height: 90vh;
   //border:1px solid black;
@@ -35,6 +42,7 @@ const ChatContaier = styled(Container)`
   .typing-dot:nth-of-type(3) {
     animation-delay: 0.25s;
   }
+
   @keyframes typing-dot {
     15% {
       transform: translateY(-35%);
@@ -44,11 +52,25 @@ const ChatContaier = styled(Container)`
       transform: translateY(0%);
       opacity: 1;
     }
+    
+  }
+  .customMessage{
+    svg{
+      display:none;
+    }
+  }
+ 
+  @media screen and (max-width:500px) {
+     // max-width: fit-content;
+     grid-template-rows: 12% 80% 8%;
+      //background-color: black;
+      
+    
   }
 `;
 
 const TitleContainer = styled(Container)`
-  height: 100%;
+  height: 85%;
   width: 100%;
   border: 1px solid white;
   background-color: ${colors.primary};
@@ -59,22 +81,39 @@ const TitleContainer = styled(Container)`
     margin-left: 10px;
     align-items: center;
   }
+  @media screen and (max-width:500px) {
+     height:100%;
+     min-height: 60px;
+      margin-bottom: 1rem;
+    
+  }
 `;
+
+const MessageContainer =  styled.div`
+   // background-color: red;
+   @media screen and (max-width:500px) {
+    padding-top: 0.5rem;
+
+    
+  }
+`
 
 const InputContainer = styled(Form)`
   display: flex;
   height: 100%;
   width: 100%;
+  //border:1px solid red;
   margin: auto !important;
   padding: 0px 0px;
   margin-top: 4px;
-  margin-bottom: 0px;
+  margin-bottom: 5px;
 `;
 
 const CustomInput = styled.input`
   display: flex;
   padding: 0 10px !important;
   height: 90%;
+  min-height: 50px;
   border: 1px solid #b4afaf;
   margin: auto !important;
   border-radius: ${styles.borderRadius};
@@ -90,6 +129,7 @@ const CustomInput = styled.input`
 
 const SelfMessage = styled.div`
   display: grid;
+ // border:1px solid red;
   //border:5px solid pink;
   grid-template-columns: 1fr 30px;
   background-color: ${colors.primary};
@@ -106,6 +146,7 @@ const SelfMessage = styled.div`
 
 const OtherMessage = styled.div`
   display: grid;
+  //border:1px solid red;
   grid-template-columns: 1fr 30px;
   background-color: ${colors.primaryLight};
   color: ${colors.white};
@@ -145,13 +186,35 @@ const TimeTag = styled.p`
   padding-left: 9px;
 `;
 
+const NotificationTile = styled.div`
+  display: flex;
+  flex-direction: column;
+  width:90%;
+  margin:auto;
+  text-align:justify;
+  border-radius: ${styles.borderRadius};
+
+.text{
+  margin:auto 5px; 
+}
+.message{
+  margin-left:45px;
+}
+
+  `
+
+  
+
+
 export default function Chat({ user, chats, messageStore }) {
   const location = useLocation();
   const username = location.state.username;
   const history = useHistory();
-  const [message, setMessage] = useState();
+  const [socketMessage, setSocketMessage] = useState();
+  const [notification,setNotification]=useState();
+  const [notificationModal,setNotificationModal]=useState(false);
   const [locationKeys, setLocationKeys] = useState([]);
-  const [selectedChat, setSelectedChat] = useState();
+  const [selectedChat, setSelectedChat] = useState([]);
   const [newMessage, setNewMessage] = useState();
   const [typing, setTyping] = useState(false);
   const room = location.state.id;
@@ -159,21 +222,34 @@ export default function Chat({ user, chats, messageStore }) {
 
 
   const sendMessage = async () => {
+    const currentTime = new Date();
+    let currentHours = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    if (currentHours < 10) {
+      currentHours = "0" + currentHours;
+    }
+    if (currentMinute < 10) {
+      currentMinute = "0" + currentMinute;
+    }
+    const time = currentHours + ":" + currentMinute;
+    console.log(time);
+    const Message = {
+      sender: user.currentUser.id,
+      time: time,
+      message: socketMessage,
+    };
+    console.log(selectedChat);
+    setSelectedChat([...selectedChat, Message]);
+    setSocketMessage("");
+
     const res = await messageStore.sendMessageStore(
-      message,
+      socketMessage,
       user.currentUser.id,
       room,
       receiver_id
     );
     console.log(res);
-    const Message = {
-      sender: res.sender,
-      time: res.time,
-      message: res.message,
-    };
-    setSelectedChat([...selectedChat, Message]);
-    setMessage("");
-
+   
     setTyping(false);
   };
 
@@ -182,7 +258,7 @@ export default function Chat({ user, chats, messageStore }) {
       to: receiver_id,
       from: user.currentUser.id,
     });
-    setMessage(e.target.value);
+    setSocketMessage(e.target.value);
   };
 
   const getSelectedChat = async () => {
@@ -200,20 +276,60 @@ export default function Chat({ user, chats, messageStore }) {
 
   useEffect(() => {
     messageStore.socket.on("msg-recieve", (data) => {
-      const newItem = {
-        message: data.data.message,
-        sender: data.data.from,
-        time: data.data.time,
-      };
-      setNewMessage(newItem);
-      //chats.getChats();
-      setTyping(false);
+      if(data.data.from===receiver_id)
+      {
+        const newItem = {
+          message: data.data.message,
+          sender: data.data.from,
+          time: data.data.time,
+        };
+        setNewMessage(newItem);
+        //chats.getChats();
+        setTyping(false);
+      }
+      else{
+      
+       message.open(
+         {
+          style:{
+            display: "flex",
+            flexDirection: "column",
+            width:"90%",
+            margin:"auto",
+            textAlign:"justify",
+            borderRadius:`${styles.borderRadius}`
+            
+            
+          },
+          className:"customMessage",
+          duration: 3,
+           content:<NotificationTile>
+             <FlexContainer>
+             <Avatar src={AVARTAR_SRC} size={"large"}></Avatar>
+             <Text  className="text">{username}</Text>
+             </FlexContainer>
+             <Text className="message">{data.data.message}</Text>
+           </NotificationTile>
+          
+         }
+         
+       )
+      
+      }
+     
     });
   }, []);
 
   useEffect(() => {
     messageStore.socket.on("typing-receive", (data) => {
-      setTyping(true);
+      console.log(data);
+      console.log(receiver_id)
+      if(data.data.from === receiver_id)
+      {
+        setTyping(true)
+      }
+
+     // setTyping(true);
     });
   }, []);
 
@@ -245,7 +361,7 @@ export default function Chat({ user, chats, messageStore }) {
   return (
     <ChatContaier style={{ height: "100%" }}>
       <TitleContainer>
-        <Avatar size={"large"}></Avatar>
+        <Avatar src={AVARTAR_SRC} size={"large"}></Avatar>
         <Text className="text">{username}</Text>
       </TitleContainer>
       <ScrollableFeed>
@@ -276,7 +392,8 @@ export default function Chat({ user, chats, messageStore }) {
           </TypingAnimation>
         )}
       </ScrollableFeed>
-
+      
+    
       <InputContainer>
         <CustomInput
           style={{
@@ -287,7 +404,7 @@ export default function Chat({ user, chats, messageStore }) {
             margin: "0 auto",
             borderRadius: "20px",
           }}
-          value={message}
+          value={socketMessage}
           onChange={(e) => handleMessageChange(e)}
         />
         <Button
@@ -296,7 +413,8 @@ export default function Chat({ user, chats, messageStore }) {
             color: `${colors.white}`,
             width: "10%",
             height: "90%",
-            borderRadius: "10px",
+            minHeight:"50px",
+          //  borderRadius: "10px",
             margin: "auto 3px",
           }}
           onClick={sendMessage}
@@ -304,6 +422,8 @@ export default function Chat({ user, chats, messageStore }) {
           <SendOutlined />
         </Button>
       </InputContainer>
+     
+     
     </ChatContaier>
   );
 }
